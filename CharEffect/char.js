f25f57fd08f170f1b71a121de0c2e7cb884a5762
Beo Hoang <beohoang98@users.noreply.github.com>
2017-06-05 14:49:58 +0700

    3d effect

--- CharEffect/char.js
+++ CharEffect/char.js
@@ -1,19 +1,26 @@
-var strWidth = 15;
+var strWidth = 28;
 var strHeight= 5;
-var z_depth = 100;
+var z_depth = 500;
+var gZ = 0.05;
 
 var offX = 5;
 var offY = 5;
 
-var sclWidth  = 800/(strWidth+offX*2);
-var sclHeight = 600/(strHeight+offY*2);
-var sclZ = 10;
+var sclWidth  	= 800/(strWidth+offX*2);
+var sclHeight 	= 800/(strHeight+offY*2);
+var scl 		= Math.min(sclHeight, sclWidth);
+var sclZ 		= 15;
 
-var strData =  [1,1,0,0,1,1,1,0,1,1,1,0,1,0,1,
-				1,0,1,0,1,0,0,0,0,1,0,0,1,0,1,
-				1,1,0,0,1,1,1,0,0,1,0,0,1,1,1,
-				1,0,1,0,1,0,0,0,0,1,0,0,1,0,1,
-				1,1,1,0,1,1,1,0,0,1,0,0,1,0,1];
+var PI = 3.141592698;
+
+var rX = 0;
+var rY = 0;
+
+var strData =  [1,1,0,0,1,1,1,0,1,1,1,0,1,0,1,0,0,1,0,0,1,0,0,1,0,1,0,1,
+				1,0,1,0,1,0,0,0,0,1,0,0,1,0,1,0,1,0,1,0,1,1,0,1,0,1,0,1,
+				1,1,0,0,1,1,1,0,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,1,0,0,1,0,
+				1,0,1,0,1,0,0,0,0,1,0,0,1,0,1,0,1,0,1,0,1,0,1,1,0,0,1,0,
+				1,1,1,0,1,1,1,0,0,1,0,0,1,0,1,0,1,0,1,0,1,0,0,1,0,0,1,0];
 
 function Article(z_begin) {
 	this.index	= Math.floor(Math.random()*(strWidth*strHeight));
@@ -23,31 +30,41 @@ function Article(z_begin) {
 	this.colorG = 0;
 	this.colorB = Math.random()*128+128;
 	this.z		= z_begin;
+	this.zspeed = 0;
 
 	this.update = function() {
-		if (this.z < -z_depth*sclZ) 
+		if (strData[this.index]===0) return;
+		if (this.z < 0) 
 		{
-			this.index	= Math.floor(Math.random()*(strWidth*strHeight));
-			this.x		= this.index%strWidth;
-			this.y		= Math.floor(this.index/strWidth);
-			this.z 		= z_depth*sclZ;
+			this.zspeed += gZ;
 		}
-		else 
-			this.z-= sclZ;
+		else {
+			this.zspeed -= gZ;
+			this.zspeed *= 0.96;
+		}
+		this.z += this.zspeed*sclZ;
 	}
 
 	this.show = function() {
 		if (strData[this.index]===0) return;
 		push();
-		fill(this.colorR, this.colorG, this.colorB, 128);
+		//fill(this.colorR, this.colorG, this.colorB, 255);
+		specularMaterial(this.colorR, this.colorG, this.colorB);
+
+		var eZ = this.z;
+		rotateX(rY);
+		rotateY(rX);
 		
-		var eZ = Math.max(this.z,-z_depth*sclZ/2);
-		translate(-400, -300, eZ);
-		
+		/*translate(-400, -300, eZ);
 		ellipse((this.x + offX)*sclWidth,
 				(this.y + offY)*sclHeight,
 				sclWidth,
 				sclHeight);
+		*/
+		translate((this.x + offX)*scl-400, 
+				(this.y + offY)*scl-300,
+				eZ);
+		box(scl);
 		pop();
 	}
 }
--- CharEffect/sketch.js
+++ CharEffect/sketch.js
@@ -1,17 +1,28 @@
-var n = strData.length*5;
+var n = strData.length*6;
 var article = [];
+var a_rX = 0;
+var a_rY = 0;
+var percon = 0.9;
+var oldMouseX, oldMouseY;
+var Sensitive = 10/100;
 
 function setup() {
-	createCanvas(800,600,WEBGL);
+	createCanvas(800,800,WEBGL);
 	background(0,0,0);
 	frameRate(30);
 	noStroke();
 
 	for (var i = 0; i < n; ++i) {
-		var z = (Math.random()*2-1)*z_depth*sclZ
+		var z = (Math.random()*2-1)*z_depth*sclZ;
 		var tmp = new Article(z);
 		article.push(tmp);
 	}
+
+	oldMouseX = mouseX;
+	oldMouseY = mouseY;
+
+	ambientLight(150);
+	pointLight(255, 255, 255, 0, 0, 0);
 }
 
 function draw() {
@@ -25,6 +36,11 @@ function update() {
 	{
 		article[i].update();
 	}
+	rX += a_rX;
+	rY += a_rY;
+
+	a_rX *= percon;
+	a_rY *= percon;
 }
 
 function show() {
@@ -32,4 +48,16 @@ function show() {
 	{
 		article[i].show();
 	}
+}
+
+function mousePressed() {
+	oldMouseX = mouseX;
+	oldMouseY = mouseY;
+}
+
+function mouseDragged() {
+	a_rX = (mouseX - oldMouseX)*PI*Sensitive/18;
+	a_rY = (mouseY - oldMouseY)*PI*Sensitive/18;
+	oldMouseX = mouseX;
+	oldMouseY = mouseY;
 }
