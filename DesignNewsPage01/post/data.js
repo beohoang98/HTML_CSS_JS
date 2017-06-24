9bf4aee0201d1f4cc2d6f6d9d40bcfb2a8690a28
Beo Hoang <beohoang98@users.noreply.github.com>
2017-06-24 11:20:53 +0700

    add DesignNewsPage01

---
+++ DesignNewsPage01/post/data.js
@@ -0,0 +1,15 @@
+var dataType = ['image','title','sub','tag','date'];
+var dataNews = [
+        [
+            'post/image/news01.jpg',
+            'Mexico vs Nga: 22h00 ngay 24/6',
+            '#khac#thegioi#bongdaTheGioi',
+            new Date('24-Jun-2017')
+        ],
+        [
+            'post/image/news02.jpg',
+            'Salah chuyen den Liverpool',
+            '#chuyennhuong#bongdaAnh',
+            new Date('22-Jun-2017')
+        ]
+];
